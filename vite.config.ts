fb0f059000d619e0ae2d87052f17d5d66cd7edb6
pengda.ma <pengda.ma@weimob.com>
2021-03-20 22:12:34 +0800

    feat: github pub page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+const name = require('./package.json').name;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.CI ? name : './',
   plugins: [reactRefresh()]
 })
